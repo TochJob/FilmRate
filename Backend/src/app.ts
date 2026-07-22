fc763bede13ba80aaf22242a245ac5cf6c947d6d
TochJob <tochjob@gmail.com>
2026-07-22 14:04:24 +0300

    feat: update app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import { authRoutes } from './routes/auth.js'
+import fastifyCookie from '@fastify/cookie'
 
 export async function buildApp() {
   const app = fastify({ logger: true })
+  await app.register(fastifyCookie)
 
   await app.register(authRoutes)
   app.get('/helth', async (request, reply) => {
