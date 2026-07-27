11ddc29f1e5bab2091ed1b6b29bfef99859ae651
TochJob <tochjob@gmail.com>
2026-07-27 15:05:01 +0300

    feat: add route to app

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { authRoutes } from './routes/auth.js'
+import { movieRoutes } from './routes/movie.js'
 import fastifyCookie from '@fastify/cookie'
 
 export async function buildApp() {
@@ -8,6 +9,8 @@ export async function buildApp() {
   app.decorateRequest('currentUser', null)
 
   await app.register(authRoutes)
+  await app.register(movieRoutes)
+  
   app.get('/helth', async (request, reply) => {
     return { status: 'ok' }
   })
